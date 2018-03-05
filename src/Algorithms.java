8864e10bc9ec749a0964da0b057c44d52885bc2c
atrubey <alext@trubey.net>
2018-03-05 13:16:07 -0800

    Finished the first 5 questions.

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,4 +1,78 @@
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Algorithms {
+	
+	public static int findBrokenEgg(List<String> eggs) {
+		
+		int firstCrackedIndex = -1; 
+		
+		for (int i = 0; i < eggs.size(); i++) {
+			if (eggs.get(i).equals("cracked")) {
+				firstCrackedIndex = i; 
+				break; 
+			}	
+		}
+		
+		return firstCrackedIndex; 
+		
+	}
 
+	public static int countPearls(List<Boolean> oysters) {
+		
+		int numPearls = 0; 
+		
+		for (int i = 0; i < oysters.size(); i++) {
+			if (oysters.get(i) == true) {
+				numPearls++; 
+			}	
+		}
+		
+		return numPearls; 
+		
+	}
+	
+	public static double findTallest(List<Double> peeps) {
+		
+		int tallestIndex = 0; 
+		
+		for (int i = 0; i < peeps.size(); i++) {
+			if (peeps.get(i) > peeps.get(tallestIndex)) {
+				tallestIndex = i; 
+			}	
+		}
+		
+		return peeps.get(tallestIndex); 
+		
+	}
+		
+	public static String findLongestWord(List<String> words) {
+		
+		int longestIndex = 0; 
+		
+		for (int i = 0; i < words.size(); i++) {
+			if (words.get(i).length() > words.get(longestIndex).length()) {
+				longestIndex = i; 
+			}	
+		}
+		
+		return words.get(longestIndex); 
+		
+	}
+	
+	public static boolean containsSOS(List<String> message) {
+		
+		boolean containsSOS = false; 
+		
+			if (message.contains("... --- ...")) {
+				containsSOS = true; 
+			}	
+		
+		return containsSOS; 
+		
+	}
+	
 }
